709bfcc1a7225de2fa2b219a684c54471bc7b521
AstickDutta <astickdutta19@gmail.com>
2022-08-13 20:22:18 +0530

    mongoose

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect ('mongodb+srv://Astick_Dutta:AavSNrGfPyPswMGg@cluster0.laksbb0.mongodb.net/Astick-DB'
 , {
    useNewUrlParser: true 
 }
 ).then( () => {console.log( "MongoDb is connected")}  )
 .catch( err => console.log(err))
+
 
 
 
