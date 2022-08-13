c2df83b00a4db360524d0bb97c02900a8662b45e
AstickDutta <astickdutta19@gmail.com>
2022-08-14 00:14:37 +0530

    schema assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBookAdd", UserController.createBook)
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBooksData", UserController.getBookData)
 
 module.exports = router;
+
+
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
