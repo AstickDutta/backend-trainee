5502ac17d8f1a26416a2d28738d404e5081c14f1
AstickDutta <astickdutta19@gmail.com>
2022-09-02 13:25:33 +0530

    assigmment axios-2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController");
-
+const weather= require("../controllers/weather.js")
 
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.get("/cowin/getByDistrictId",CowinController.getByDistrictId)
+router.get("/londonWeather",weather.londonWeather)
+router.get("/weatherOfMentionedCities", weather.weatherc)
 
 module.exports = router;
